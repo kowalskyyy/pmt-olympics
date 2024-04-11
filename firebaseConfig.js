8b7583cb9b9acef2fa52ddf62f117086807684f9
Bartosz Kowalski <bartosz.kowalski@pinmeto.com>
2024-04-11 19:24:29 +0200

    remove console.log

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -9,16 +9,14 @@ import { getFirestore } from "firebase/firestore"; // For Firestore
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
-
-console.log(process.env.API_KEY);
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
