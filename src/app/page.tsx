b3e0abdb9cba403fd2a1af4f37f808412d084ad2
Lybron Sobers <lybron.sobers@pinmeto.com>
2024-04-12 10:39:05 +0200

    Dashboard: add photo feed next to dashboard as scrollable list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,32 @@
 "use client";
 
-import { Typography, Container } from "@mui/material";
+import { Grid, Typography, Container } from "@mui/material";
 import NavBar from "../app/components/navbar";
+import PhotoFeed from "@/app/components/photofeed";
 import UserScores from "@/app/components/userscores";
 
 export default function HomePage() {
   return (
     <>
       <NavBar navButton="My account" url="/my-account" />
-      <Container maxWidth="sm" sx={{ mt: 2 }}>
-        <Typography variant="h2" component="h1" gutterBottom>
-          Digital Scoreboard
-        </Typography>
-        <UserScores />
-        <Typography variant="body1">
-          This is where the scores will be displayed.
-        </Typography>
-      </Container>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <Container maxWidth="sm" sx={{ mt: 2 }}>
+            <Typography variant="h2" component="h1" gutterBottom>
+              Digital Scoreboard
+            </Typography>
+            <UserScores />
+            <Typography variant="body1">
+              This is where the scores will be displayed.
+            </Typography>
+          </Container>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <div style={{ height: "100vh", overflow: "auto", maxWidth: "400px" }}>
+            <PhotoFeed />
+          </div>
+        </Grid>
+      </Grid>
     </>
   );
 }
