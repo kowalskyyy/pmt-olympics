35ff523c7a5c0a9a80ebc1de4cf40fb21352a00a
Bartosz Kowalski <bartosz.kowalski@pinmeto.com>
2024-04-12 11:29:11 +0200

    fix image refetching

--- src/app/components/photofeed.jsx
+++ src/app/components/photofeed.jsx
@@ -10,6 +10,7 @@ import {
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import LinearProgress from "@mui/material/LinearProgress";
+import { useAuthContext } from "../../context/AuthContext";
 import Alert from "@mui/material/Alert";
 
 import {
@@ -19,10 +20,14 @@ import {
   getDownloadURL,
   listAll,
 } from "firebase/storage";
+import { db } from "../../../firebaseConfig"; 
+import { collection, doc, setDoc, onSnapshot } from "firebase/firestore";
 
 export default function PhotoFeed() {
   const storage = getStorage();
   const [image, setImage] = useState(null);
+  const { user, loading } = useAuthContext();
+
   const [images, setImages] = useState([]);
 
   const [open, setOpen] = useState(false);
@@ -67,6 +72,9 @@ export default function PhotoFeed() {
       () => {
         getDownloadURL(uploadTask.snapshot.ref).then((url) => {
           setImages([...images, url]);
+          // Add the image URL to the 'images' field in the UserScore collection
+          const userScoreRef = doc(collection(db, "UserScore"), user.uid); // Replace "user_id" with the actual user ID
+          setDoc(userScoreRef, { images: { [img.name]: url } }, { merge: true });
         });
         console.log(`Uploaded ${img.name}`);
         setUploading(false);
@@ -75,17 +83,22 @@ export default function PhotoFeed() {
   };
 
   useEffect(() => {
-    const fetchImages = async () => {
-      const imagesRef = ref(storage, "images");
-      const result = await listAll(imagesRef);
-      let urlPromises = result.items.map((imageRef) =>
-        getDownloadURL(imageRef)
-      );
-      Promise.all(urlPromises).then((urls) => {
-        setImages(urls);
+    const userScoreCollectionRef = collection(db, "UserScore");
+    const unsubscribe = onSnapshot(userScoreCollectionRef, (querySnapshot) => {
+      const allImages = [];
+      querySnapshot.forEach((doc) => {
+        const data = doc.data();
+        if (data && data.images) {
+          allImages.push(...Object.values(data.images));
+        }
       });
+      setImages(allImages);
+    });
+  
+    // Clean up the listener when the component unmounts
+    return () => {
+      unsubscribe();
     };
-    fetchImages();
   }, []);
 
   const handleClickOpen = (url) => {
